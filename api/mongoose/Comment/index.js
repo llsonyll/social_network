aaf03e40fc2da4c8127617ef6d5e67bd87fe4c5c
llsonyll <sony_s07@hotmail.es>
2022-07-19 11:15:22 -0500

    feat(mongoose): comment, post, review and user schemas

---
+++ api/mongoose/Comment/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commentsSchema = void 0;
+const mongoose_1 = require("mongoose");
+exports.commentsSchema = new mongoose_1.Schema({
+    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, unique: true },
+    postId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'Post' },
+    userId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'User' },
+    content: { type: String, required: true },
+});
---
+++ api/mongoose/User/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+let userSchema = new mongoose_1.Schema({
+    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, unique: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    password: { type: String, required: true },
+    posts: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'Post', required: true }],
+    following: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true }],
+    followers: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true }],
+    isAdmin: { type: Boolean, default: false, required: true },
+    isPremium: { type: Boolean, default: false, required: true },
+    isPrivate: { type: Boolean, default: false, required: true },
+    isConnected: Boolean,
+    birthday: Date,
+    biography: String,
+    review: { type: mongoose_1.Schema.Types.ObjectId, ref: 'Review' }
+});
