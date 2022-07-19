0f72cd9e633caefe9adfb00e75a372cc91bd5917
llsonyll <sony_s07@hotmail.es>
2022-07-19 16:20:54 -0500

    feat(layout): Dashboard, Messages, Profile and Home pages

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,6 +1,12 @@
 import "./navbar.css";
 
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
+  let activeStyle = {
+    fontWeight: "bold",
+  };
+
   return (
     <div className="navbar flex justify-between px-12 py-2">
       <div className="home center">SN</div>
@@ -8,18 +14,24 @@ const NavBar = () => {
         <input type="text" placeholder="Search a friend" />
       </div>
       <div className="actions">
-        <div className="option-text">
-          {/* icon */}
-          Profile
-        </div>
-        <div className="option-text">
+        <NavLink to="">
           {/* icon */}
           Home
-        </div>
-        <div className="option-text">
+        </NavLink>
+        <NavLink
+          to="profile"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          {/* icon */}
+          Me
+        </NavLink>
+        <NavLink
+          to="messages"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
           {/* icon */}
           Messages
-        </div>
+        </NavLink>
         <div className="option-btn">
           <button> + </button>
         </div>
---
+++ client/src/pages/Messages/messages.jsx
@@ -0,0 +1,11 @@
+import "./messages.css";
+
+const Messages = () => {
+  return (
+    <div>
+      <h1> Messages </h1>
+    </div>
+  );
+};
+
+export default Messages;
