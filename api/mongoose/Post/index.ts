aec42574ef0040c8b3e7f8fad7235847b5506f21
Zeus Vargas <zeusvargas26@gmail.com>
2022-07-19 14:08:05 -0500

    feat: (front) add dependencies

--- api/mongoose/Post/index.ts
+++ api/mongoose/Post/index.ts
@@ -1,27 +1,32 @@
-import {Schema} from "mongoose"
-import { IPost } from "../../types"
+import { Schema } from 'mongoose'
+import { IPost } from '../../types'
 
-
-export let postSchema = new Schema <IPost> ({
-   _id: {
-     type: Schema.Types.ObjectId,
-     required: true,
-     unique: true
-   },
-   userId:{
-     type: Schema.Types.ObjectId,
-     ref:'Post'
-   },
-   commentsId: [{
-     type: Schema.Types.ObjectId,
-     ref:'Comment'
-   }],
-   likes:[{
-    types: Schema.Types.ObjectId,
-    ref:'User'
-   }],
-   dislikes: [{
-    types: Schema.Types.ObjectId,
-    ref:'User'
-   }]
+export let postSchema = new Schema<IPost>({
+	_id: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		unique: true,
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'Post',
+	},
+	commentsId: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Comment',
+		},
+	],
+	likes: [
+		{
+			types: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	],
+	dislikes: [
+		{
+			types: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	],
 })
