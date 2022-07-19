8c3b9b7bc4ef491d829a4fd0eaf107ed110824bb
llsonyll <sony_s07@hotmail.es>
2022-07-19 16:58:08 -0500

    feat(asset): SN logo and navbar logo updated

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { FaHome, FaUserCircle, FaFacebookMessenger } from "react-icons/fa";
 
 import NewPostBtn from "../NewPostBtn";
+import logoSN from "../../../assets/LogoSN.png";
 
 const NavBar = () => {
   let activeStyle = {
@@ -18,7 +19,7 @@ const NavBar = () => {
   return (
     <div className="navbar flex justify-between px-12 py-3 items-center">
       <div className="flex items-center">
-        <div className="home center">SN</div>
+        <img src={logoSN} alt="logoSN" className="h-6" />
         <div className="search-box ml-10">
           <input
             className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-1 pl-2 pr-7 border-transparent bg-gray-700 text-white sm:text-sm rounded-md"
