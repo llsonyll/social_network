5d1207da1c33528ac8b79d7f6c04ebcf461806a2
llsonyll <sony_s07@hotmail.es>
2022-07-19 15:50:49 -0500

    feat: home-landing routes, mock-data and tailwind config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
-import Landing from './pages/landing'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import "./App.css";
+import Landing from "./pages/landing";
+import Home from "./pages/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Landing />} />
-				<Route path='/home' element={<Landing />} />
-				<Route path='/profile' element={<Landing />} />
-				<Route path='/chat' element={<Landing />} />
-				<Route path='/postDetailPage' element={<Landing />} />
-				<Route path='/team' element={<Landing />} />
-			</Routes>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Landing />} />
+        <Route path="/chat" element={<Landing />} />
+        <Route path="/postDetailPage" element={<Landing />} />
+        <Route path="/team" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
