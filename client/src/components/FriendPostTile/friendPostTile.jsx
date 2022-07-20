09e0e0187eeff531dfc6d98e4b3f71b4a24cc98c
llsonyll <sony_s07@hotmail.es>
2022-07-20 15:44:42 -0500

    feat(component): Following/Friends state bar completed

--- client/src/components/FriendPostTile/friendPostTile.jsx
+++ client/src/components/FriendPostTile/friendPostTile.jsx
@@ -1,10 +1,38 @@
 import "./friendPostTile.css";
 
-const FriendPostTile = () => {
+const FriendPostTile = ({ tile }) => {
+  // console.log(user);
+
+  const getTimePublished = () => {
+    // TODO: considerar no solo horas sino dias-meses-años
+    const minutes = tile.publishedAt;
+    if (minutes <= 1) return "1 minute ago";
+    if (minutes < 60) return `${minutes} minutes ago`;
+    if (minutes / 60 <= 1.5) return "1 hour ago";
+    return `${Math.round(minutes / 60)} hours ago`;
+  };
+
+  const handleTileClick = () => {
+    // TODO: receive a function as prop or just redirect to PostDetail with ID
+    console.log(tile.postId);
+  };
+
   return (
-    <div className="tile">
-      <h1>FriendPostTile</h1>
-    </div>
+    <button className="flex w-full my-3 py-1" onClick={handleTileClick}>
+      <div className="rounded-full w-12 h-12 p-1 bg-white overflow-hidden">
+        <img
+          className="object-cover h-full rounded-full"
+          src={tile.user.url}
+          alt="avatarIMG"
+        />
+      </div>
+      <div className="px-2 flex flex-col justify-start items-start ">
+        <div className="text-white text-lg font-bold">{tile.user.name}</div>
+        <div className="text-opacity-50 text-white text-sm font-normal leading-3">
+          {getTimePublished()}
+        </div>
+      </div>
+    </button>
   );
 };
 
--- client/src/pages/PostDetail/postDetail.jsx
+++ client/src/pages/PostDetail/postDetail.jsx
@@ -2,6 +2,36 @@ import "./postDetail.css";
 
 import FriendPostTile from "../../components/FriendPostTile";
 
+const dummyFriendPost = [
+  {
+    postId: "1",
+    publishedAt: 2,
+    user: {
+      id: "1",
+      name: "Juanito",
+      url: "https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+    },
+  },
+  {
+    postId: "2",
+    publishedAt: 45,
+    user: {
+      id: "1",
+      name: "Juanito",
+      url: "https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+    },
+  },
+  {
+    postId: "3",
+    publishedAt: 120,
+    user: {
+      id: "2",
+      name: "Fernandito",
+      url: "https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+    },
+  },
+];
+
 const PostDetail = () => {
   const containerStyle = {
     backgroundColor: "#2e2e2e",
@@ -10,9 +40,10 @@ const PostDetail = () => {
   return (
     <div className="flex-1 flex px-11 pt-11 gap-8" style={containerStyle}>
       <div className="bg-stone-800 basis-1/4 p-6 flex flex-col items-center rounded-md">
-        <div className="text-white font-semibold text-xl"> Your Friends</div>
-
-        <FriendPostTile />
+        <div className="text-white font-medium text-xl mb-8"> Following </div>
+        {dummyFriendPost.map((tile) => (
+          <FriendPostTile tile={tile} key={tile.postId} />
+        ))}
       </div>
       <div className="bg-stone-800 basis-3/4 p-6 rounded-md">
         <div className=""> Your Friends</div>
