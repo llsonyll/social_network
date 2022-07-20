a3dc427bca1cbcc6f9463bb68343af2f5b2657ff
llsonyll <sony_s07@hotmail.es>
2022-07-20 18:01:29 -0500

    feat(component): Avatar renders s,m and large sizes also PostDetails and Navbar responsive features

--- client/src/components/Avatar/avatar.jsx
+++ client/src/components/Avatar/avatar.jsx
@@ -1,8 +1,25 @@
 const Avatar = ({
   imgUrl = "https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+  size = "s", // 's', 'm', 'l'
 }) => {
-  return (
-    <div className="rounded-full w-12 h-12 p-1 bg-white overflow-hidden">
+  return size === "s" ? (
+    <div className={`rounded-full bg-white overflow-hidden p-1 w-8 h-8`}>
+      <img
+        className="object-cover h-full rounded-full"
+        src={imgUrl}
+        alt="avatarIMG"
+      />
+    </div>
+  ) : size === "m" ? (
+    <div className={`rounded-full bg-white overflow-hidden p-1 w-10 h-10`}>
+      <img
+        className="object-cover h-full rounded-full"
+        src={imgUrl}
+        alt="avatarIMG"
+      />
+    </div>
+  ) : (
+    <div className={`rounded-full bg-white overflow-hidden p-1 w-12 h-12`}>
       <img
         className="object-cover h-full rounded-full"
         src={imgUrl}
--- client/src/pages/landing/landing.jsx
+++ client/src/pages/landing/landing.jsx
@@ -1,86 +1,88 @@
-import './landing.css'
-import {Link} from 'react-router-dom'
-//logo
-import Logo from '../../../assets/LogoSN.png'
+import "./landing.css";
+import { Link } from "react-router-dom";
 
+//Logo
+import Logo from "../../../assets/LogoSN.png";
 
 //iconos
-import {BsGoogle} from 'react-icons/bs'
-import {AiFillFacebook} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiFillStar} from 'react-icons/ai'
+import { BsGoogle } from "react-icons/bs";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 
 const Landing = () => {
-	return (
-		<div className='landing_container'>
-			<div className="wrap_toggle">
-	
-				<input id='toggle'type="checkbox" name=""  />
-				<label for='toggle'className='switch' htmlFor="toggle"></label>
-				<div className='text_toggle'>
-				<p>Sign in</p>
-				<img src={Logo} alt="" />
-				<p className='register_text'>Register</p>
-				</div>
-			</div>
-			<form action=''>
-				<div className="logo">
-					<img src={Logo} alt="" />
-					<h1>Sign in now</h1>
-				</div>
-				<div className='input_container'>
-					<div className='buttons_container'>
-						<button>Sign in</button>
-						<button>Register</button>
-					</div>
-					<div className='email_container'>
-						<label htmlFor=''>Email</label>
-						<input type='text' name='' id='' placeholder='Email'/>
-					</div>
-					<div className='password_container'>
-						<label htmlFor=''>Password</label>
-						<input type='password' name='' id='' placeholder='Password'/>
-					</div>
-					<div className='checkbox_container'>
-						<div className='remember_container'>
-							<input type="checkbox" name="" id="" /> 
-							<span>Remember me</span>
-						</div>
-						<Link to=''>Forgot your password?</Link>
-					</div>
-					<button className='on' type='submit'>
-						Sign in
-					</button>
-					<div className='orcontinue'>
-						<div className='line'></div>
-						<div className='text_line'>Or continue with</div>
-						<div className='line'></div>
-					</div>
-					<div className='social_buttons'>
-						<button>
-							<BsGoogle/>
-						</button>
-						<button>
-							<AiFillFacebook/>
-						</button>
-						<button>
-							<AiOutlineTwitter/>
-						</button>
-					</div>
-				</div>
-			</form>
-			<div className='sn_review'>
-				<div className='star'>
-					<AiFillStar/>
-					<span>5</span>
-				</div>
-				<div>
-				<h3>Sundar Pichai</h3>
-				<p>"This is the app that I've found to create long lasting relationships"</p>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="landing_container">
+      <div className="wrap_toggle">
+        <input id="toggle" type="checkbox" name="" />
+        <label for="toggle" className="switch" htmlFor="toggle"></label>
+        <div className="text_toggle">
+          <p>Sign in</p>
+          <img src={Logo} alt="" />
+          <p className="register_text">Register</p>
+        </div>
+      </div>
+      <form action="">
+        <div className="logo">
+          <img src={Logo} alt="" />
+          <h1>Sign in now</h1>
+        </div>
+        <div className="input_container">
+          <div className="buttons_container">
+            <button>Sign in</button>
+            <button>Register</button>
+          </div>
+          <div className="email_container">
+            <label htmlFor="">Email</label>
+            <input type="text" name="" id="" placeholder="Email" />
+          </div>
+          <div className="password_container">
+            <label htmlFor="">Password</label>
+            <input type="password" name="" id="" placeholder="Password" />
+          </div>
+          <div className="checkbox_container">
+            <div className="remember_container">
+              <input type="checkbox" name="" id="" />
+              <span>Remember me</span>
+            </div>
+            <Link to="">Forgot your password?</Link>
+          </div>
+          <button className="on" type="submit">
+            Sign in
+          </button>
+          <div className="orcontinue">
+            <div className="line"></div>
+            <div className="text_line">Or continue with</div>
+            <div className="line"></div>
+          </div>
+          <div className="social_buttons">
+            <button>
+              <BsGoogle />
+            </button>
+            <button>
+              <AiFillFacebook />
+            </button>
+            <button>
+              <AiOutlineTwitter />
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="sn_review">
+        <div className="star">
+          <AiFillStar />
+          <span>5</span>
+        </div>
+        <div>
+          <h3>Sundar Pichai</h3>
+          <p>
+            "This is the app that I've found to create long lasting
+            relationships"
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
