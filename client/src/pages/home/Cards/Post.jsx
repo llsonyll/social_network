5af6155031ea3b58f4bd08f3a3004d6e8127322c
alberto012 <rechavarria2@gmail.com>
2022-07-20 01:02:45 -0300

    home 70%! without logic

---
+++ client/src/pages/home/Cards/Post.jsx
@@ -0,0 +1,18 @@
+import "./Post.css"
+export default function Post(){
+    return(
+
+    <div className="notifD">
+    <img
+            className="imagenPost"
+            src="https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg"
+          />
+      <h1 className="name">user</h1>
+      <div className="post">
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, vitae odio dolor officiis, quae possimus corrupti tenetur, nemo quidem adipisci dignissimos? Corporis fugiat eius eligendi voluptatibus rerum quia maiores quo!</p>
+      </div>
+  
+    </div>
+    )
+}
