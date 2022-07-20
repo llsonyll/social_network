fbd7aa90e86fd08b6f3fcf7ccddd30836e2e8557
unknown <jonathanlaguado99@gmail.com>
2022-07-20 15:02:04 -0500

    feat(routes route)added cors and types of cors

--- api/routes/route.js
+++ api/routes/route.js
@@ -6,7 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Para las rutas en general
 const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./auth/index"));
+const cors_1 = __importDefault(require("cors"));
 const server = (0, express_1.default)();
+const options = {
+    allowedHeaders: [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-Access-Token',
+        'Authorization'
+    ],
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+};
+server.use((0, cors_1.default)(options));
 server.use(express_1.default.json());
 // server.use("/auth",(req,res)=>authRouter.get(req,res));
 server.use("/auth", index_1.default);
--- api/routes/route.ts
+++ api/routes/route.ts
@@ -1,8 +1,26 @@
 // Para las rutas en general
-import express from 'express'
-import authRouter from "./auth/index"
+import express from 'express';
+import authRouter from "./auth/index";
+import cors from "cors";
 
 const server = express();
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+    'Authorization'
+  ],
+  credentials: true,
+  origin: "http://localhost:3000",
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+};
+
+server.use(cors(options))
 
 server.use(express.json());
 
