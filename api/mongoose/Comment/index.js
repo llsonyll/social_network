6ce4291167c95f89b5bf8ebcf200582934552412
Francisco <franciscopischetz@gmail.com>
2022-07-20 00:49:26 -0300

    fix(mongoose-schemas) schemas compiled

--- api/mongoose/Comment/index.js
+++ api/mongoose/Comment/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.commentsSchema = void 0;
 const mongoose_1 = require("mongoose");
 exports.commentsSchema = new mongoose_1.Schema({
-    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, unique: true, auto: true },
+    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, auto: true },
     postId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'Post' },
     userId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'User' },
     content: { type: String, required: true },
--- api/mongoose/User/index.js
+++ api/mongoose/User/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.userSchema = void 0;
 const mongoose_1 = require("mongoose");
 exports.userSchema = new mongoose_1.Schema({
-    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, unique: true, auto: true },
+    _id: { type: mongoose_1.Schema.Types.ObjectId, required: true, auto: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     firstname: { type: String, required: true },
