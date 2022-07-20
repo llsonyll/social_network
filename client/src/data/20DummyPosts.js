65627a91f42e90d860fc43bd4d23dff56185985e
Zeus Vargas <zeusvargas26@gmail.com>
2022-07-19 21:31:16 -0500

    feat: add 20 users and 20 posts dummy + generators

---
+++ client/src/data/20DummyPosts.js
@@ -0,0 +1,764 @@
+export const mockPost = [
+	[
+		{
+			_id: 3.3716157051173496,
+			userId: {
+				_id: 3334534.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312664.37161570514,
+					userId: {
+						_id: 236785.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32110.371615705117,
+					userId: {
+						_id: 12268.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864658.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126783.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 4.37161570511735,
+			userId: {
+				_id: 3334535.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312665.37161570514,
+					userId: {
+						_id: 236786.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32111.371615705117,
+					userId: {
+						_id: 12269.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864659.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126784.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 5.37161570511735,
+			userId: {
+				_id: 3334536.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312666.37161570514,
+					userId: {
+						_id: 236787.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32112.371615705117,
+					userId: {
+						_id: 12270.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864660.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126785.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 6.37161570511735,
+			userId: {
+				_id: 3334537.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312667.37161570514,
+					userId: {
+						_id: 236788.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32113.371615705117,
+					userId: {
+						_id: 12271.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864661.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126786.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 7.37161570511735,
+			userId: {
+				_id: 3334538.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312668.37161570514,
+					userId: {
+						_id: 236789.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32114.371615705117,
+					userId: {
+						_id: 12272.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864662.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126787.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 8.37161570511735,
+			userId: {
+				_id: 3334539.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312669.37161570514,
+					userId: {
+						_id: 236790.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32115.371615705117,
+					userId: {
+						_id: 12273.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864663.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126788.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 9.37161570511735,
+			userId: {
+				_id: 3334540.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312670.37161570514,
+					userId: {
+						_id: 236791.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32116.371615705117,
+					userId: {
+						_id: 12274.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864664.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126789.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 10.37161570511735,
+			userId: {
+				_id: 3334541.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312671.37161570514,
+					userId: {
+						_id: 236792.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32117.371615705117,
+					userId: {
+						_id: 12275.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864665.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126790.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 11.37161570511735,
+			userId: {
+				_id: 3334542.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312672.37161570514,
+					userId: {
+						_id: 236793.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32118.371615705117,
+					userId: {
+						_id: 12276.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864666.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126791.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 12.37161570511735,
+			userId: {
+				_id: 3334543.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312673.37161570514,
+					userId: {
+						_id: 236794.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32119.371615705117,
+					userId: {
+						_id: 12277.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864667.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126792.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 13.37161570511735,
+			userId: {
+				_id: 3334544.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312674.37161570514,
+					userId: {
+						_id: 236795.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32120.371615705117,
+					userId: {
+						_id: 12278.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864668.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126793.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 14.37161570511735,
+			userId: {
+				_id: 3334545.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312675.37161570514,
+					userId: {
+						_id: 236796.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32121.371615705117,
+					userId: {
+						_id: 12279.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864669.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126794.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 15.37161570511735,
+			userId: {
+				_id: 3334546.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312676.37161570514,
+					userId: {
+						_id: 236797.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32122.371615705117,
+					userId: {
+						_id: 12280.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864670.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126795.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 16.37161570511735,
+			userId: {
+				_id: 3334547.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312677.37161570514,
+					userId: {
+						_id: 236798.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32123.371615705117,
+					userId: {
+						_id: 12281.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864671.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126796.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 17.37161570511735,
+			userId: {
+				_id: 3334548.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312678.37161570514,
+					userId: {
+						_id: 236799.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32124.371615705117,
+					userId: {
+						_id: 12282.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864672.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126797.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 18.37161570511735,
+			userId: {
+				_id: 3334549.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312679.37161570514,
+					userId: {
+						_id: 236800.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32125.371615705117,
+					userId: {
+						_id: 12283.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864673.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126798.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 19.37161570511735,
+			userId: {
+				_id: 3334550.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312680.37161570514,
+					userId: {
+						_id: 236801.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32126.371615705117,
+					userId: {
+						_id: 12284.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864674.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126799.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 20.37161570511735,
+			userId: {
+				_id: 3334551.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312681.37161570514,
+					userId: {
+						_id: 236802.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32127.371615705117,
+					userId: {
+						_id: 12285.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864675.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126800.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 21.37161570511735,
+			userId: {
+				_id: 3334552.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312682.37161570514,
+					userId: {
+						_id: 236803.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32128.371615705117,
+					userId: {
+						_id: 12286.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864676.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126801.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+		{
+			_id: 22.37161570511735,
+			userId: {
+				_id: 3334553.371615705,
+				username: 'pepe',
+			},
+			content: 'blablabla',
+			comments: [
+				{
+					_id: 312683.37161570514,
+					userId: {
+						_id: 236804.3716157051,
+						username: 'aaaaaa',
+					},
+					content: 'Me encantó tu post!!!!',
+				},
+				{
+					_id: 32129.371615705117,
+					userId: {
+						_id: 12287.371615705117,
+						username: 'cccccc',
+					},
+					content: 'Como desveo esta cosa?',
+				},
+			],
+			likes: [
+				{
+					_id: 23864677.371615704,
+					username: 'aaaaaa',
+				},
+			],
+			dislikes: [
+				{
+					_id: 126802.37161570512,
+					username: 'cccccc',
+				},
+			],
+		},
+	],
+]
