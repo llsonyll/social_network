6f2d0fa00f94fa65f78562a9fe21fc9a9a083048
llsonyll <sony_s07@hotmail.es>
2022-07-21 11:40:22 -0500

    style(upgrade): bigger icon, better colors and non-resize for textarea

--- client/src/components/NewPost/newPost.jsx
+++ client/src/components/NewPost/newPost.jsx
@@ -14,7 +14,7 @@ const NewPost = ({ showModal, setShowModal }) => {
         <div className="relative rounded-lg shadow bg-[#363636]">
           <button
             type="button"
-            className="absolute top-2 right-1 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+            className="absolute top-2 right-1 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 "
             data-modal-toggle="popup-modal"
             onClick={() => setShowModal(false)}
           >
@@ -23,17 +23,17 @@ const NewPost = ({ showModal, setShowModal }) => {
             </span>
           </button>
           <div className="py-5 px-6 lg:px-8 flex">
-            <Avatar size="m" />
+            <Avatar size="l" />
             <form className="space-y-8 flex-1 mx-3" action="#">
               <textarea
                 id="message"
                 rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2.5 w-full text-sm bg-transparent rounded-lg border-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 resize-none"
                 placeholder="Your message..."
               ></textarea>
 
               <div className=" relative flex items-baseline justify-between after:content-[''] after:ml-0 after:absolute after:right-0 after:left-0 after:-top-2 after:bg-[#424242] after:h-0.5">
-                <button className="text-white">
+                <button className="text-white text-2xl">
                   <FaImage />
                 </button>
                 <button className="bg-green-600 text-white py-1 px-7 rounded-md shadow-lg text-sm">
