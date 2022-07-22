f5fa252364beb66318c01188cf67881f78c24dba
Francisco <franciscopischetz@gmail.com>
2022-07-21 23:27:46 -0300

    feat(redux auth) added action for login an user

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 
 export const loginAction = (obj) => async (dispatch) => {
     try{
-        console.log(obj)
         let res = await axios.post('http://localhost:3001/auth/login', obj)
         res = res.data
-        console.log(res)
         localStorage.setItem('token', res.token)
         return dispatch(loginUser({username: res.username, _id:res._id}))
     }catch(err){
