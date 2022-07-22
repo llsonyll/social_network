849971c76c959f28149bf272194676edbda677af
Francisco <franciscopischetz@gmail.com>
2022-07-21 23:38:50 -0300

    feat(authActions redux) added registerAction to connect back auth

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -11,3 +11,14 @@ export const loginAction = (obj) => async (dispatch) => {
         console.log(err)
     }
 }
+
+export const registerAction = (obj) => async (dispatch) =>{
+    try{
+        let res = await axios.post('http://localhost:3001/auth/register', obj)
+        res = res.data
+        localStorage.setItem('token', res.token)
+        return dispatch(loginUser({username: res.username, _id: res._id}))
+    }catch(err){
+        console.log(err)
+    }
+}
