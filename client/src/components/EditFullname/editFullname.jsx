7188466b3b5c2337200f60ff697896a5cbcef0d9
llsonyll <sony_s07@hotmail.es>
2022-07-24 18:05:08 -0500

    reafactor: profile names

--- client/src/components/EditFullname/editFullname.jsx
+++ client/src/components/EditFullname/editFullname.jsx
@@ -1,96 +1,105 @@
-import '../../pages/Profile'
-import { useState } from 'react'
-import { AiFillCloseSquare } from 'react-icons/ai'
-import Avatar from '../Avatar'
-import { useDispatch, useSelector } from 'react-redux'
-import { modifyUser } from '../../redux/actions/userActions'
+import "../../pages/Profile";
+import { useState } from "react";
+import { AiFillCloseSquare } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useDispatch, useSelector } from "react-redux";
+import { modifyUser } from "../../redux/actions/userActions";
 
 const EditFullname = ({ renderChangeRenderComponents, user }) => {
-	const loggedUser = useSelector(store => store.auth.loggedUser)
-	const dispatch = useDispatch()
-	const handleOnCancel = () => {
-		renderChangeRenderComponents('firstname')
-	}
-	//DESDE AQUI SE PONEN LAS ACCIONES
-	const [firstname, setFirstname] = useState('')
-	const [lastname, setLastname] = useState('')
+  const loggedUser = useSelector((store) => store.auth.loggedUser);
+  const { firstname: currentFirstName, lastname: currentLastName } =
+    useSelector((state) => state.user.userProfileData);
+  const dispatch = useDispatch();
+  const handleOnCancel = () => {
+    renderChangeRenderComponents("firstname");
+  };
+  //DESDE AQUI SE PONEN LAS ACCIONES
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
 
-	const handleOnSubmit = (e) => {
-		e.preventDefault()
-		if (firstname.length > 0 && lastname.length === 0) {
-			dispatch(modifyUser(loggedUser._id, {firstname}))
-			alert('You changed your name!')
-			renderChangeRenderComponents('firstname')
-		}
-		else if (firstname.length === 0 && lastname.length > 0) {
-			dispatch(modifyUser(loggedUser._id, {lastname}))
-			alert('You changed your name!')
-			renderChangeRenderComponents('firstname')
-		}
-		else if (firstname.length > 0 && lastname.length > 0) {
-			dispatch(modifyUser(loggedUser._id, {firstname,lastname}))
-			alert('You changed your name!')
-			renderChangeRenderComponents('firstname')
-		}
-		else if (firstname.length === 0 && lastname.length === 0) {
-			alert('Fill any input before submitting')
-		}
-	}
+  // no espacios, no numeros, 10 fn 10 ln caracteres
 
-	return (
-		<div
-			data-modal-placement='top-middle'
-			className={` absolute inset-0 z-50 bg-black/40 items-center justify-center overflow-y-auto overflow-x-hidden flex`}>
-			<div className='relative p-4 w-full max-w-xl h-full md:h-auto'>
-				<div className='relative rounded-lg shadow bg-[#363636]'>
-					<div className='text-white text-center pt-10 px-2 text-xl'> Write a new name and submit it</div>
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const firstNameNoSpaces = firstname.trim();
+    const lastNameNoSpaces = lastname.trim();
+    if (firstname.length > 0 && lastname.length === 0) {
+      dispatch(modifyUser(loggedUser._id, { firstname }));
+      alert("You changed your name!");
+      renderChangeRenderComponents("fullname");
+    } else if (firstname.length === 0 && lastname.length > 0) {
+      dispatch(modifyUser(loggedUser._id, { lastname }));
+      alert("You changed your lastName!");
+      renderChangeRenderComponents("fullname");
+    } else if (firstname.length > 0 && lastname.length > 0) {
+      dispatch(modifyUser(loggedUser._id, { firstname, lastname }));
+      alert("You changed your full-name!");
+      renderChangeRenderComponents("fullname");
+    } else if (firstname.length === 0 && lastname.length === 0) {
+      alert("Fill any input before submitting");
+    }
+  };
 
-					<button
-						type='button'
-						className='absolute top-2 right-1 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 '
-						data-modal-toggle='popup-modal'
-						onClick={() => handleOnCancel()}>
-						<span className='text-2xl'>
-							<AiFillCloseSquare />
-						</span>
-					</button>
-					<div className='py-5 px-6 lg:px-8 flex'>
-						<form className='space-y-8 flex-1 mx-3' action='#'>
-							<input
-								id='message'
-								rows='4'
-								onChange={(e) => {
-									e.preventDefault()
-									setFirstname(e.target.value)
-								}}
-								className='block outline-none bg-stone-800 p-2.5 w-full text-sm bg-transparent rounded-lg border-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 resize-none'
-								placeholder={`New First name`}></input>
-							<input
-								id='message'
-								rows='4'
-								onChange={(e) => {
-									e.preventDefault()
-									setLastname(e.target.value)
-								}}
-								className='block outline-none bg-stone-800 p-2.5 w-full text-sm bg-transparent rounded-lg border-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 resize-none'
-								placeholder={`New Last name`}></input>
+  return (
+    <div
+      data-modal-placement="top-middle"
+      className={` absolute inset-0 z-50 bg-black/40 items-center justify-center overflow-y-auto overflow-x-hidden flex`}
+    >
+      <div className="relative p-4 w-full max-w-xl h-full md:h-auto">
+        <div className="relative rounded-lg shadow bg-[#363636]">
+          <div className="text-white text-center pt-10 px-2 text-xl">
+            Change your info and update it
+          </div>
 
-							<div className=" relative flex text-center items-baseline justify-center after:content-[''] after:ml-0 after:absolute after:right-0 after:left-0 after:-top-2 after:bg-[#424242] after:h-0.5">
-								<button
-									onClick={(e) => {
-										e.preventDefault()
-										handleOnSubmit(e)
-									}}
-									className='bg-green-600 text-white  mt-5 py-2 px-8 rounded-md shadow-lg text-sm  transition-all hover:scale-105'>
-									Submit
-								</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+          <button
+            type="button"
+            className="absolute top-2 right-1 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 "
+            data-modal-toggle="popup-modal"
+            onClick={() => handleOnCancel()}
+          >
+            <span className="text-2xl">
+              <AiFillCloseSquare />
+            </span>
+          </button>
 
-export default EditFullname
+          <div className="py-5 px-6 lg:px-8 flex">
+            <form className="space-y-8 flex-1 mx-3" action="#">
+              <input
+                type="text"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setFirstname(e.target.value);
+                }}
+                className="block outline-none bg-stone-800 p-2.5 w-full text-sm bg-transparent rounded-lg border-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 resize-none"
+                placeholder={currentFirstName}
+              ></input>
+              <input
+                rows="4"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setLastname(e.target.value);
+                }}
+                className="block outline-none bg-stone-800 p-2.5 w-full text-sm bg-transparent rounded-lg border-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 resize-none"
+                placeholder={currentLastName}
+              ></input>
+
+              <div className=" relative flex text-center items-baseline justify-center after:content-[''] after:ml-0 after:absolute after:right-0 after:left-0 after:-top-2 after:bg-[#424242] after:h-0.5">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleOnSubmit(e);
+                  }}
+                  className="bg-green-600 text-white  mt-5 py-2 px-8 rounded-md shadow-lg text-sm  transition-all hover:scale-105"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditFullname;
--- client/src/pages/Profile/profile.jsx
+++ client/src/pages/Profile/profile.jsx
@@ -1,187 +1,195 @@
-import './profile.css'
-import { UsersDummy } from '../../data/20UsersDummy'
-import { Fragment, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import EditFullname from '../../components/EditFullname'
-import EditUsername from '../../components/EditUsername'
-import EditBiography from '../../components/EditBiography'
-import ProfilePosts from '../../components/ProfilePostsRenderer'
-import { mockPost } from '../../data/20DummyPosts'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { getUserProfile } from '../../redux/actions/userActions'
+import "./profile.css";
+import { UsersDummy } from "../../data/20UsersDummy";
+import { Fragment, useState } from "react";
+import { useParams } from "react-router-dom";
+import EditFullname from "../../components/EditFullname";
+import EditUsername from "../../components/EditUsername";
+import EditBiography from "../../components/EditBiography";
+import ProfilePosts from "../../components/ProfilePostsRenderer";
+import { mockPost } from "../../data/20DummyPosts";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getUserProfile } from "../../redux/actions/userActions";
 
 const Profile = () => {
-	const params = useParams()
-	const [firstname, setFirstname] = useState(false)
-	const [username, setUsername] = useState(false)
-	const [biography, setBiography] = useState(false)
-	const [image, setImage] = useState(false)
-	const userLogged = useSelector((state) => state.auth.loggedUser._id)
-	const userData = useSelector((state) => state.user.userProfileData)
-	const dispatch = useDispatch()
+  const params = useParams();
+  const [firstname, setFirstname] = useState(false);
+  const [username, setUsername] = useState(false);
+  const [biography, setBiography] = useState(false);
+  const [image, setImage] = useState(false);
+  const userLogged = useSelector((state) => state.auth.loggedUser._id);
+  const userData = useSelector((state) => state.user.userProfileData);
+  const dispatch = useDispatch();
 
-	const renderChangeRenderComponents = (nameOfTheComponentToRender) => {
-		if (nameOfTheComponentToRender === 'firstname') {
-			setFirstname(false)
-		}
-		if (nameOfTheComponentToRender === 'username') {
-			setUsername(false)
-		}
-		if (nameOfTheComponentToRender === 'biography') {
-			setBiography(false)
-		}
-		if (nameOfTheComponentToRender === 'image') {
-			setImage(false)
-		}
-	}
-	//traigo la info del perfil en el q estoy (didMount)
-	useEffect(() => {
-		dispatch(getUserProfile(params.id))
-	}, [params.id])
+  const renderChangeRenderComponents = (nameOfTheComponentToRender) => {
+    if (nameOfTheComponentToRender === "fullname") {
+      setFirstname(false);
+    }
+    if (nameOfTheComponentToRender === "username") {
+      setUsername(false);
+    }
+    if (nameOfTheComponentToRender === "biography") {
+      setBiography(false);
+    }
+    if (nameOfTheComponentToRender === "image") {
+      setImage(false);
+    }
+  };
 
-	function getTimeOfCreation(date) {
-		let now = new Date().getTime()
-		let created = new Date(date).getTime()
-		const minutes = (now - created) / 60000
-		if (minutes <= 1) return '1 minute ago'
-		if (minutes < 60) return `${Math.round(minutes)} minutes ago`
-		if (minutes / 60 <= 1.5) return '1 hour ago'
-		if (minutes / 60 > 24 && minutes / 60 <= 36) return '1 day ago'
-		if (minutes / 60 > 36) return `${Math.round(minutes / (60 * 24))} days ago`
-		return `${Math.round(minutes / 60)} hours ago`
-	}
+  //traigo la info del perfil en el q estoy (didMount)
+  useEffect(() => {
+    dispatch(getUserProfile(params.id));
+  }, [params.id]);
 
-	let user = userData
-	console.log(user)
-	let userPosts = user.posts
-	let renderer = () => {
-		if (userPosts.length > 0) {
-			return userPosts.map((p) => {
-				return (
-					<Fragment key={p.postNumber}>
-						
-							<ProfilePosts
-								userId={p.userId._id}
-								postNumber={p._id}
-								fullname={`${user.firstname + ' ' + user.lastname}`}
-								timeAgo={getTimeOfCreation(p.createdAt)}
-								commentsLength={p.commentsId.length}
-								likesLength={p.likes.length}
-								content={p.content}
-							/>
-						
-					</Fragment>
-				)
-			})
-		}
-	}
+  function getTimeOfCreation(date) {
+    let now = new Date().getTime();
+    let created = new Date(date).getTime();
+    const minutes = (now - created) / 60000;
+    if (minutes <= 1) return "1 minute ago";
+    if (minutes < 60) return `${Math.round(minutes)} minutes ago`;
+    if (minutes / 60 <= 1.5) return "1 hour ago";
+    if (minutes / 60 > 24 && minutes / 60 <= 36) return "1 day ago";
+    if (minutes / 60 > 36) return `${Math.round(minutes / (60 * 24))} days ago`;
+    return `${Math.round(minutes / 60)} hours ago`;
+  }
 
-	return (
-		<>
-			<div className='p-container'>
-				<div className='profile-container'>
-					<div className='img-container'>
-						{/* <img
-								className='profile-img'
-								src='https://japanpowered.com/media/images//goku.png'
-								alt='Profile Picture'>
-							</img> */}
-						<div className='img_profile'>
-							<div className='info'>
-								<p>Change Image</p>
-							</div>
-						</div>
-					</div>
-					<div className='shadow-box'>
-						<div className='user_description'>
-							<div className='user-firstname'>
-								<div className='info_container'>
-									<span className='span-info'>Full name</span>
-									<p>{`${user.firstname + ' ' + user.lastname}`}</p>
-								</div>
-								<div className='button_container'>
-									{params.id === userLogged ? (
-										<button
-											onClick={() => {
-												setFirstname(true)
-											}}
-											type='button'>
-											Edit
-										</button>
-									) : null}
-								</div>
-							</div>
-							<div className='user-username'>
-								<div className='info_container'>
-									<span className='span-info'>Username</span>
-									{'@' + user.username}
-								</div>
-								<div className='button_container'>
-									{params.id === userLogged ? (
-										<button
-											onClick={() => {
-												setUsername(true)
-											}}
-											type='button'>
-											Edit
-										</button>
-									) : null}
-								</div>
-							</div>
+  let user = userData;
+  console.log(user);
+  let userPosts = user.posts;
+  let renderer = () => {
+    if (userPosts.length > 0) {
+      return userPosts.map((p) => {
+        return (
+          <Fragment key={p.postNumber}>
+            <ProfilePosts
+              userId={p.userId._id}
+              postNumber={p._id}
+              fullname={`${user.firstname + " " + user.lastname}`}
+              timeAgo={getTimeOfCreation(p.createdAt)}
+              commentsLength={p.commentsId.length}
+              likesLength={p.likes.length}
+              content={p.content}
+            />
+          </Fragment>
+        );
+      });
+    }
+  };
 
-							<div className='user-followers'>
-								<div className='info_container'>
-									<span className='span-info'>Followers</span>
-									{user._id ? user.followers.length : null}
-								</div>
-							</div>
-							<div className='user-following'>
-								<div className='info_container'>
-									<span className='span-info'>Following</span>
-									{user._id ? user.following.length : null}
-								</div>
-							</div>
+  return (
+    <>
+      <div className="p-container">
+        <div className="profile-container">
+          <div className="img-container">
+            {/* <img
+				className='profile-img'
+				src='https://japanpowered.com/media/images//goku.png'
+				alt='Profile Picture'>
+			</img> */}
+            <div className="img_profile">
+              <div className="info">
+                <p>Change Image</p>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-box">
+            <div className="user_description">
+              <div className="user-firstname">
+                <div className="info_container">
+                  <span className="span-info">Full name</span>
+                  <p>{`${user.firstname + " " + user.lastname}`}</p>
+                </div>
+                <div className="button_container">
+                  {params.id === userLogged ? (
+                    <button
+                      onClick={() => {
+                        setFirstname(true);
+                      }}
+                      type="button"
+                    >
+                      Edit
+                    </button>
+                  ) : null}
+                </div>
+              </div>
+              <div className="user-username">
+                <div className="info_container">
+                  <span className="span-info">Username</span>
+                  {"@" + user.username}
+                </div>
+                <div className="button_container">
+                  {params.id === userLogged ? (
+                    <button
+                      onClick={() => {
+                        setUsername(true);
+                      }}
+                      type="button"
+                    >
+                      Edit
+                    </button>
+                  ) : null}
+                </div>
+              </div>
 
-							<div className='user-biography'>
-								<div className='info_container'>
-									<span className='span-info'>Biography</span>
-									{user.biography ? user.biography : 'No bio yet'}
-								</div>
-								<div className='button_container'>
-									{params.id === userLogged ? (
-										<button
-											onClick={() => {
-												setBiography(true)
-											}}
-											type='button'>
-											Edit
-										</button>
-									) : null}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<hr />
-				<div id='Profile-posts__container'>
-				{user._id ? renderer() : null}
-				</div>
-			</div>
-			{firstname === true && (
-				<EditFullname renderChangeRenderComponents={renderChangeRenderComponents} user={user} />
-			)}
-			{username === true && (
-				<EditUsername renderChangeRenderComponents={renderChangeRenderComponents} user={user} />
-			)}
-			{biography === true && (
-				<EditBiography renderChangeRenderComponents={renderChangeRenderComponents} user={user} />
-			)}
-			{/* {image === true && <EditLastname renderChangeRenderComponents={renderChangeRenderComponents} user={user} />} */}
+              <div className="user-followers">
+                <div className="info_container">
+                  <span className="span-info">Followers</span>
+                  {user._id ? user.followers.length : null}
+                </div>
+              </div>
+              <div className="user-following">
+                <div className="info_container">
+                  <span className="span-info">Following</span>
+                  {user._id ? user.following.length : null}
+                </div>
+              </div>
 
-			{/* Espacio para mapear 20 objetos con el componente renderizador de los posts y los 20 posts que le pido a la db */}
-		</>
-	)
-}
+              <div className="user-biography">
+                <div className="info_container">
+                  <span className="span-info">Biography</span>
+                  {user.biography ? user.biography : "No bio yet"}
+                </div>
+                <div className="button_container">
+                  {params.id === userLogged ? (
+                    <button
+                      onClick={() => {
+                        setBiography(true);
+                      }}
+                      type="button"
+                    >
+                      Edit
+                    </button>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div id="Profile-posts__container">{user._id ? renderer() : null}</div>
+      </div>
+      {firstname === true && (
+        <EditFullname
+          renderChangeRenderComponents={renderChangeRenderComponents}
+          user={user}
+        />
+      )}
+      {username === true && (
+        <EditUsername
+          renderChangeRenderComponents={renderChangeRenderComponents}
+          user={user}
+        />
+      )}
+      {biography === true && (
+        <EditBiography
+          renderChangeRenderComponents={renderChangeRenderComponents}
+          user={user}
+        />
+      )}
+      {/* {image === true && <EditLastname renderChangeRenderComponents={renderChangeRenderComponents} user={user} />} */}
+      {/* Espacio para mapear 20 objetos con el componente renderizador de los posts y los 20 posts que le pido a la db */}
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
