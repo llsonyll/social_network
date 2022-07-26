5c8cca70c3877f9d27ff1944bfc85bae30405989
llsonyll <sony_s07@hotmail.es>
2022-07-26 13:07:00 -0500

    feat(mail): sned email on register added

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -17,6 +17,7 @@ const mongoose_1 = require("../../mongoose");
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const passport_1 = __importDefault(require("passport"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const nodemailer_1 = require("nodemailer");
 let router = express_1.default.Router();
 //---------------function create Token--------------------
 const createToken = (user) => {
@@ -27,19 +28,21 @@ const middlewareNewUser = (req, res, next) => __awaiter(void 0, void 0, void 0, 
     try {
         let { email, password, firstname, lastname, username } = req.body;
         const profileArray = [
-            'https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p1_anad93.png',
-            'https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p2_tj88ek.png',
-            'https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p3_dlphru.png',
-            'https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p4_zy2yhe.png',
-            'https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p5_i3n2nd.png'
+            "https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p1_anad93.png",
+            "https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p2_tj88ek.png",
+            "https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p3_dlphru.png",
+            "https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p4_zy2yhe.png",
+            "https://res.cloudinary.com/dnw4kirdp/image/upload/v1658694142/p5_i3n2nd.png",
         ];
         if (!email || !password || !firstname || !lastname || !username) {
-            return res.status(400).json({ message: 'Please, send your email and password' });
+            return res
+                .status(400)
+                .json({ message: "Please, send your email and password" });
         }
         //search if User already exists
         let user = yield mongoose_1.User.findOne({ email: email });
         if (user) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: "User already exists" });
         }
         //password encryption
         let salt = yield bcrypt_1.default.genSalt(10);
@@ -55,70 +58,117 @@ const middlewareNewUser = (req, res, next) => __awaiter(void 0, void 0, void 0, 
     }
 });
 //------------rute register-----------------------------
-router.post('/register', middlewareNewUser, passport_1.default.authenticate('local', { session: false, failureRedirect: '/auth/login' }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/register", middlewareNewUser, passport_1.default.authenticate("local", {
+    session: false,
+    failureRedirect: "/auth/login",
+}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //user return of passport strategy
     try {
         let { user } = req;
         if (user) {
             const send = user;
-            return res
-                .status(200)
-                .json({ token: createToken(user), username: send.username, _id: send._id, profilePicture: send.profilePicture });
+            const transporter = (0, nodemailer_1.createTransport)({
+                service: "gmail",
+                auth: {
+                    user: "vavatyni@gmail.com",
+                    pass: "nlsbenyeedlvxfhb",
+                },
+            });
+            const output = `
+          <p>You have a new message from SN</p>
+          <h3>Mail Details</h3>
+          <ul>  
+            <li>Nuevo Registro</li>
+          </ul>
+          <h3>Message</h3>
+          <p>Usuario creado satisfactoriamente, procede a ingresar a nuestra plataforma <a href="https://finaldeploy-tau.vercel.app" target="_blank"> </a></p>
+        `;
+            const mailOptions = {
+                from: "Social Network <vavatyni@gmail.com>",
+                to: "sony_s07@hotmail.es",
+                subject: "Sending Email using Node.js/NodeMailer with TS",
+                html: output,
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log("Email sent: " + info.response);
+                }
+            });
+            return res.status(200).json({
+                token: createToken(user),
+                username: send.username,
+                _id: send._id,
+                profilePicture: send.profilePicture,
+            });
             //res.redirect()
         }
-        return res.status(400).json('The user does not exists');
+        return res.status(400).json("The user does not exists");
     }
     catch (error) {
         res.json(error);
     }
 }));
 //route error login
-router.get('/login', (req, res) => {
-    res.status(400).json('Incorrect email or password.');
+router.get("/login", (req, res) => {
+    res.status(400).json("Incorrect email or password.");
 });
-router.get('/loginjwt', (req, res) => {
-    res.status(400).json('Token needed');
+router.get("/loginjwt", (req, res) => {
+    res.status(400).json("Token needed");
 });
 //-----------------------------login user -----------------------------
 /*
   strategy passport local, verify user in database if error redirect route error login
 */
-router.post('/login', passport_1.default.authenticate('local', { session: false, failureRedirect: '/auth/login' }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/login", passport_1.default.authenticate("local", {
+    session: false,
+    failureRedirect: "/auth/login",
+}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //user return of passport strategy
     try {
         let { user } = req;
         if (user) {
             const send = user;
-            return res
-                .status(200)
-                .json({ token: createToken(user), username: send.username, _id: send._id, profilePicture: send.profilePicture });
+            return res.status(200).json({
+                token: createToken(user),
+                username: send.username,
+                _id: send._id,
+                profilePicture: send.profilePicture,
+            });
             //res.redirect()
         }
-        return res.status(400).json('The user does not exists');
+        return res.status(400).json("The user does not exists");
     }
     catch (error) {
         res.json(error);
     }
 }));
 //------------------------route data user----------------------------------
-router.post('/', passport_1.default.authenticate('jwt', { session: false, failureRedirect: '/auth/loginjwt' }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/", passport_1.default.authenticate("jwt", {
+    session: false,
+    failureRedirect: "/auth/loginjwt",
+}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     try {
         //-------------extract Authorization from HTTP headers----------------
-        const authorization = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ');
-        if (!authorization || authorization.length !== 2 || authorization[0].toLocaleLowerCase() !== 'bearer') {
-            return res.status(400).json('no token');
+        const authorization = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(" ");
+        if (!authorization ||
+            authorization.length !== 2 ||
+            authorization[0].toLocaleLowerCase() !== "bearer") {
+            return res.status(400).json("no token");
         }
         const token = authorization[1];
         //------------------------decode token-----------------------------------
         let verifyToken = jsonwebtoken_1.default.verify(`${token}`, `${process.env.SECRET_TEST}`);
         if (!verifyToken || !verifyToken.user) {
-            res.status(401).json('Invalid Token');
+            res.status(401).json("Invalid Token");
         }
         let { id } = verifyToken.user;
         const user = yield mongoose_1.User.findById(`${id}`);
         if (!user) {
-            return res.status(400).json('Invalid Token');
+            return res.status(400).json("Invalid Token");
         }
         let { username, profilePicture } = user;
         return res.status(200).json({ _id: id, username, profilePicture });
@@ -128,7 +178,10 @@ router.post('/', passport_1.default.authenticate('jwt', { session: false, failur
     }
 }));
 //-------------------------route test---------------------------------
-router.get('/test', passport_1.default.authenticate('jwt', { session: false, failureRedirect: '/auth/loginjwt' }), (req, res) => {
-    res.json({ msg: 'Todo funciona a la perfeccion' });
+router.get("/test", passport_1.default.authenticate("jwt", {
+    session: false,
+    failureRedirect: "/auth/loginjwt",
+}), (req, res) => {
+    res.json({ msg: "Todo funciona a la perfeccion" });
 });
 exports.default = router;
