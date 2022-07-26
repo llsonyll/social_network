7461f2f52d3b0e07fe0796452c4142820b4cbc20
llsonyll <sony_s07@hotmail.es>
2022-07-25 19:04:21 -0500

    refactor: landing-signin-register

--- client/src/utils/axios/index.js
+++ client/src/utils/axios/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// import { dummyInterceptor } from "./interceptors";
 
 const API_URL = "https://dreamteamapi.herokuapp.com/";
 const timeout = 5000;
@@ -7,10 +8,6 @@ export const apiConnection = axios.create({
   baseURL: API_URL,
   timeout,
 });
-
-// apiConnection.interceptors.request.use((config) => {
-//   console.log("AXIOS INTERCEPTOR", config);
-// });
 
 export const setAuthorization = (token) =>
   (apiConnection.defaults.headers.common["Authorization"] = `Bearer ${token}`);
