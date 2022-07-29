c96515b5cbf2044dec45e57e4247fe199dabe598
Harold <harold.tenorio@epn.edu.ec>
2022-07-29 18:46:06 -0500

    Edit y delete comments

--- client/src/redux/actions/commentActions.js
+++ client/src/redux/actions/commentActions.js
@@ -1,6 +1,6 @@
 import { addPostDetail } from "../reducers/postReducer.slice";
 import { apiConnection } from "../../utils/axios";
-
+import Swal from 'sweetalert2'
 export const createComment = (userId, postId, info) => async (dispatch) => {
   try {
     const { data } = await apiConnection.post(
@@ -20,20 +20,32 @@ export const editComment = (userId, postId, commentId, info) => async (dispatch)
       `comment/${userId}/${postId}/${commentId}`,
       info
     );
-
+    Swal.fire({
+      icon: "success",
+      title: "Good job!",
+      text: 'Comment edit successfully',
+      background: "#4c4d4c",
+      color: "white",
+    });
     return dispatch(addPostDetail(data));
   } catch (error) {
     console.log(error);
   }
 };
 
-export const deleteComment = (userId, postId, commentId, info) => async (dispatch) => {
+export const deleteComment = (userId, postId, commentId) => async (dispatch) => {
   try {
     const { data } = await apiConnection.delete(
-      `comment/${userId}/${postId}/${commentId}`,
-      info
+      `comment/${userId}/${postId}/${commentId}`
+    
     );
-
+    Swal.fire({
+      icon: "info",
+      title: ":(",
+      text: 'Comment delete successfully',
+      background: "#4c4d4c",
+      color: "white",
+    });
     return dispatch(addPostDetail(data));
   } catch (error) {
     console.log(error);
