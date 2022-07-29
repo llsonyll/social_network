77f4e1ac07f1d699643b51211d961bb43546ff1e
Francisco <franciscopischetz@gmail.com>
2022-07-29 18:44:50 -0300

    feat (videoCall) mute and stop camera functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,8 @@ import Draggable from 'react-draggable'
 import io from 'socket.io-client';
 import { addMessage } from "./redux/reducers/chatReducer";
 export const socket = io('http://localhost:3001');
+let peer;
+let call;
 
 //IMPORTS PARA LLAMADAS
 
@@ -35,15 +37,11 @@ function App() {
   const loggedUser = useSelector((state) => state.auth.loggedUser);
   const remoteVideoRef = useRef()
   const localVideoRef = useRef()
+  const [actualyLogged, setActuallyLogged] = useState()
   const [myVideo, setMyVideo] = useState()
   const [otherVideo, setOtherVideo] = useState()
-	const [onCall, setOnCall] = useState(false)
-  const handleCanPlayRemote = () => {
-    remoteVideoRef.current.play();
-  }
-  const handleCanPlayLocal = () => {
-    localVideoRef.current.play();
-  }
+  const [onCall, setOnCall] = useState(false)
+
 
 
 
@@ -62,25 +60,34 @@ function App() {
     // }
   }, [location]);
 
-  //SOCKET useEffect TO REPORT A LOGGED USER
+  //SOCKET useEffect TO REPORT A LOGGED USER, AND HANDLE CALLS
   useEffect(() => {
-    if(loggedUser._id){
-		let {_id} = loggedUser
-		let id = _id
-		const peer = new Peer(id)
+	if(loggedUser._id){
+		setActuallyLogged(loggedUser._id)
+	}
+  },[loggedUser])
+
+
+  useEffect(() => {
+    if(actualyLogged){
+		peer = new Peer(actualyLogged)
 		const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 		peer.on('open', function (id) { console.log( 'I got My peer ID:' + id, peer) })
 		console.log(peer)
-      	socket.emit('logged', loggedUser._id, socket.id)
+      	socket.emit('logged', actualyLogged, socket.id)
 		socket.on('call',(_id) => {
 					setOnCall(true)
-					console.log(onCall);
 					getUserMedia(
 					{ video: true, audio: true }, function(stream){
-					console.log(stream)
-					const call = peer.call(_id, stream);
+					call = peer.call(_id, stream);
 					console.log(call)
 					setMyVideo(stream)
+					call.on('close', () => {
+						console.log('me cerraron unu 2')
+						setMyVideo('')
+						setOnCall(false)
+						call.close()
+					})
 					call.on("stream", function(remoteStream){
 						setOtherVideo(remoteStream)
 						// Show stream in some <video> element.
@@ -90,17 +97,21 @@ function App() {
 					console.error("Failed to get local stream", err);
 				},)
 		})
-		peer.on("call", (call) => {
-				setOnCall(true)
-				console.log(onCall);
-			console.log(peer.id)
-			console.log('algo')
+		peer.on("call", (calling) => {
+			setOnCall(true)
+			console.log(onCall);
+			call = calling
 			getUserMedia(
 				{ video: true, audio: true },
 				(stream) => {
-					call.answer(stream); // Answer the call with an A/V stream.
+					calling.answer(stream); // Answer the call with an A/V stream.
 					setMyVideo(stream)
-					call.on("stream", (remoteStream) => {
+					calling.on('close', () => {
+						setMyVideo('')
+						setOnCall(false)
+						calling.close()
+					})
+					calling.on("stream", (remoteStream) => {
 						setOtherVideo(remoteStream)
 						// Show stream in some <video> element.
 					});
@@ -115,7 +126,7 @@ function App() {
 		socket.off('logged')
 		socket.off('call')
 	})
-  }, [loggedUser])
+  }, [actualyLogged])
 
   //SOCKET useEFFECT TO LISTEN MESSAGES
   useEffect(() => {
@@ -129,22 +140,34 @@ function App() {
   }, [location])
 
 
-
-
   	useEffect(() => {
 		if(otherVideo){
 			remoteVideoRef.current.srcObject = otherVideo
-			remoteVideoRef.current.onloadedmetadata = function(e) {videos.current.play()}
+			remoteVideoRef.current.onloadedmetadata = function(e) {remoteVideoRef.current.play()}
 		}
 	},[otherVideo])
 
 	useEffect(()=>{
 		if(myVideo){
 			localVideoRef.current.srcObject = myVideo
-			localVideoRef.current.onloadedmetadata = function(e) {videos.current.play()}
+			localVideoRef.current.onloadedmetadata = function(e) {localVideoRef.current.play()}
 		}
+		console.log('holaaaa')
 	},[myVideo])
 
+	const handleCloseChat = () => {
+		// myVideo.getVideoTracks().forEach(track => track.enabled = !track.enabled)
+		myVideo.getVideoTracks().forEach(track => track.stop())
+		myVideo.getAudioTracks().forEach(track => track.stop())
+	}
+
+	const handleStopCamera = () => {
+		
+	}
+
+	function handleMuteMic() {
+		myVideo.getAudioTracks().forEach(track => track.enabled = !track.enabled)
+	}
 
 	return (
 			<>
@@ -155,12 +178,12 @@ function App() {
 				<div id="video_container">
 					<div className="user_videos_container">
 					<video ref={remoteVideoRef} autoPlay muted/>
-					<video ref={localVideoRef}  autoPlay muted/>
+					<video ref={localVideoRef}  autoPlay />
 
 					</div>
 					<div className="buttons_video_container">
-						<button> <FiPhoneMissed/> </button>
-						<button><AiOutlineAudioMuted/>  </button>
+						<button onClick={handleCloseChat}> <FiPhoneMissed/> </button>
+						<button onClick={handleMuteMic}><AiOutlineAudioMuted />  </button>
 					</div>
 				</div>
 			</Draggable> : null
