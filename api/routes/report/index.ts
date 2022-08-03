51e8c1894d28840bcc690644a6c1935c5fc87641
ElPepo97 <pedroumido@outlook.com>
2022-08-03 00:36:24 -0300

    feat(DELETE /report) added delete route for comments and posts reports

--- api/routes/report/index.ts
+++ api/routes/report/index.ts
@@ -8,7 +8,7 @@ router.post('/:userId/:reportId', passport.authenticate('jwt', {session:false, f
 async (req: Request, res: Response) => {
     try {
         const { userId, reportId } = req.params;
-        const { reason, reported } = req.body; // REPORTED VA A ACEPTAR 3 VALORES: COMMENT, POST Y USER
+        const { reason, reported } = req.body;
 
         if(!reason){
             return res.status(404).json({msg: 'Not Reason'})
@@ -64,11 +64,15 @@ try{
     let reports: any[] = []
     if(!type) {
         reports = await Report.find({})
+        .populate('commentReportedId', 'userId')
+        .populate('postReportedId', 'userId')
     }
     if(type === "postReportedId") {
         reports = await Report.find({postReportedId: {$exists: true} })
+        .populate('postReportedId', 'userId')
     } if(type==="commentReportedId") {
         reports = await Report.find({commentReportedId: {$exists: true}})
+        .populate('commentReportedId', 'userId')
     } if(type==="userReportedId") {
         reports = await Report.find({userReportedId: {$exists: true}})
     }
@@ -79,6 +83,72 @@ try{
     res.status(400).json({errMsg: err})
 }
 
-})
+});
+
+router.delete('/:userId/:reportId', passport.authenticate('jwt', {session:false, failureRedirect: '/auth/loginjwt'}),
+async (req:Request, res:Response) =>{
+    try{
+        const { userId, reportId } = req.params;
+        const { type } = req.body;
+
+        const admin = await User.findById(`${userId}`);
+        if(!admin || !admin.isAdmin) return res.status(401).json('Missings permissions');
+        
+        const report = await Report.findById(`${reportId}`);
+        
+        if(!report) return res.status(404).json('Report not found');
+        
+        if (type === 'post') {
+            const post = await Post.findById(`${report.postReportedId}`);
+            if (!post) return res.status(404).json('Post not found');
+            console.log(post)
+            
+            const user = await User.findById(`${post.userId}`);
+            if (!user) return res.status(404).json('User not found');
+            console.log(user)
+            
+            const newUser = await User.findOneAndUpdate({_id: user._id}, {$pull: {posts: `${post._id}`}}, {new: true});
+            if (!newUser) return res.status(400).json('Not posible to delete');
+            console.log(newUser)
+            await newUser.save();
+            
+            const commentId = post.commentsId;
+            await Comment.deleteMany({_id: {$in: commentId}});
+            await report.remove();
+            await post.remove();
+            
+            return res.json('Reported successfully');
+        }
+        if (type === 'comment') {
+            const comment = await Comment.findById(`${report.commentReportedId}`);
+            if (!comment) return res.status(404).json('Comment not found');
+            console.log(comment)
+
+            const post = await Post.findById(comment.postId);
+            if (!post) return res.status(404).json('Post not found');
+            console.log(post)
+
+            const newPost = await Post.findOneAndUpdate({_id: post._id}, {$pull: {commentsId: comment._id}}, {new: true});
+            if (!newPost) return res.status(400).json('Not posible to delete');
+            await newPost.save();
+            console.log(newPost)
+            await report.remove();
+            await comment.remove();
+            console.log('todo ok')
+
+            return res.json('Reported successfully');
+        }
+
+        // const report = await User.findById(`${reportId}`);
+        // if (!report) return res.status(404).json('User not found');
+
+        // report.isDeleted = true;
+        // await report.save();
+        
+        return res.json('Reported successfully');
+    }catch(err){
+        return res.status(400).json('Something went wrong');
+    }
+});
 
 export default router;
