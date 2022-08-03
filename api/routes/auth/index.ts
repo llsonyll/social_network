0a81c1a403409bee22f347f3901449613d3179c5
unknown <jonathanlaguado99@gmail.com>
2022-08-03 08:42:08 -0500

    fix(cookie)

--- api/routes/auth/index.ts
+++ api/routes/auth/index.ts
@@ -182,7 +182,7 @@ router.get(
 
       const send: IUser = user as IUser;
 
-      res.cookie("token", createToken(user as IUser),{domain:`www.dream-team-api.social`, httpOnly: true, secure: true ,path:"/"});
+      res.cookie("token", createToken(user as IUser),{domain:`client-beta-mauve.vercel.app`, httpOnly: true, secure: true ,path:"/"});
       return res.redirect(`${process.env.URL_FRONT}`);
     } catch (err) {
       res.status(400).json({ err: "todo salio mal" });
