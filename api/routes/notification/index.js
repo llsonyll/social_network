ff638c7d809ec980e091c359078b1e490f22ac1f
Tomas <tomas8olivera@gmail.com>
2022-08-03 00:11:46 -0300

    feat(routes): to post, get, and put

--- api/routes/notification/index.js
+++ api/routes/notification/index.js
@@ -35,10 +35,46 @@ router.post('/:fromId/:toId', passport_1.default.authenticate("jwt", {
             content: content,
             seen: false,
         });
+        yield notification.save();
         return res.json({ msg: "Notification posted successfully", notification });
     }
     catch (err) {
         return res.status(400).json({ errorMsg: err });
+    }
+}));
+router.get('/:userId', passport_1.default.authenticate("jwt", {
+    session: false,
+    failureRedirect: "/auth/loginjwt",
+}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    try {
+        const user = yield mongoose_1.User.findById(`${userId}`);
+        if (!user)
+            return res.status(404).json({ errorMsg: "Wtf who are you men?" });
+        const notifications = yield mongoose_1.Notification.find({ to: user._id });
+        return res.send(notifications);
+    }
+    catch (err) {
+        return res.status(400).json({ errorMsg: err });
+    }
+}));
+router.put('/seen/:userId', passport_1.default.authenticate("jwt", {
+    session: false,
+    failureRedirect: "/auth/loginjwt",
+}), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    const date = new Date().getTime();
+    try {
+        const user = yield mongoose_1.User.findById(`${userId}`);
+        if (!user)
+            return res.status(404).json({ errorMsg: "Wtf who are you?" });
+        const seenNotifications = yield mongoose_1.Notification.updateMany({ to: user._id }, { $set: { seen: true } }, { new: true });
+        const deleted = yield mongoose_1.Notification.deleteMany({ to: user._id, createdAt: { $lt: new Date(date - 259200000) }, seen: true });
+        const updatedNotifications = yield mongoose_1.Notification.find({ to: user._id });
+        return res.send(updatedNotifications);
+    }
+    catch (err) {
+        res.status(400).json({ errorMsg: err });
     }
 }));
 exports.default = router;
