11af8fb2fdbb5fb9accd02c43a7f4874b8f5f385
unknown <jonathanlaguado99@gmail.com>
2022-08-03 19:25:24 -0500

    merge

--- client/src/components/ProfilePostsRenderer/profilePosts.jsx
+++ client/src/components/ProfilePostsRenderer/profilePosts.jsx
@@ -8,10 +8,6 @@ import { Link } from 'react-router-dom'
 import { Fragment, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import Swal from 'sweetalert2';
-import EditPost from '../EditPost.jsx/editPost';
-import ListOfUsersRenderer from '../ListOfUsersRenderer/listOfUsersRenderer';
-
 import {
   newDislikeUserProfile,
   newLikeUserProfile,
